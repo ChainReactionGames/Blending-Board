f553be3e7c68775aecdb3e15319bcae0c754ed86
Brayden Gogis <btgogis@gmail.com>
2020-07-06 17:42:46 -0400

    Beginning of card design and swiping

---
+++ Blending Board/LetterCard.swift
@@ -0,0 +1,40 @@
+//
+//  LetterCard.swift
+//  Blending Board
+//
+//  Created by Brayden Gogis on 7/6/20.
+//
+
+import UIKit
+
+class LetterCard: UIView {
+    @IBOutlet weak var blur: UIVisualEffectView!
+    @IBOutlet weak var tintView: UIView!
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var letterField: UITextField!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    func setup() {
+        layer.cornerCurve = .continuous
+        layer.cornerRadius = 15
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(panInSelf(_:)))
+        self.addGestureRecognizer(pan)
+    }
+    @objc func panInSelf(_ pan: UIPanGestureRecognizer) {
+        let trans = pan.translation(in: self.superview)
+        switch pan.state {
+        case .ended:
+            UIView.animate(withDuration: Double(sqrt(pow(trans.x, 2) + pow(trans.y, 2)) / 250)) {
+                self.transform = .identity
+            }
+        default:
+            self.transform = CGAffineTransform(translationX: trans.x, y: trans.y).rotated(by: pow(abs(trans.x), 1) / 600 * (trans.x < 0 ? -.pi : .pi))
+        }
+    }
+}
