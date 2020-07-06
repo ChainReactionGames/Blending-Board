5e53a251e94fa7b0631791f83e13736cf4ff6bdc
Brayden Gogis <btgogis@gmail.com>
2020-07-06 11:09:42 -0400

    Initial Commit

---
+++ Blending Board/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blending Board
+//
+//  Created by Brayden Gogis on 7/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Blending BoardTests/Blending_BoardTests.swift
@@ -0,0 +1,33 @@
+//
+//  Blending_BoardTests.swift
+//  Blending BoardTests
+//
+//  Created by Brayden Gogis on 7/6/20.
+//
+
+import XCTest
+@testable import Blending_Board
+
+class Blending_BoardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
